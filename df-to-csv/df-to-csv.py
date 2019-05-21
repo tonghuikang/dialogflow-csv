e1e95dbe8ba67710426d7b31f3b57a87772073d5
hkgcp <hkmac@df-and-csv.c.ubtech-216703.internal>
2019-05-21 04:13:18 +0000

    multiple responses

--- df-to-csv/df-to-csv.py
+++ df-to-csv/df-to-csv.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 import glob
@@ -12,8 +12,10 @@ import os
 from pprint import pprint as pp
 
 
-# In[ ]:
+# In[2]:
 
+
+# loading testing template
 import sys
 myargs = sys.argv
 if '-f' in myargs:
@@ -22,15 +24,23 @@ if '-f' in myargs:
         print(e)
         FILE_NAME = 'template'
 else:
+    os.system("cp ./template.zip ./temp/")
+    FILE_NAME = 'template'
+# if testing on jupyter notebook
+if FILE_NAME[0] == "/":
+    os.system("cp ./template.zip ./temp/")
     FILE_NAME = 'template'
 
 
-os.system("rm -rf ./template")
+# In[3]:
+
+
+os.system("rm -rf ./temp/template")
 os.system("unzip temp/{}.zip -d temp/{}".format(FILE_NAME, FILE_NAME))
 os.system("tree")
 
 
-# In[ ]:
+# In[4]:
 
 
 files = sorted([intent for intent in glob.glob('temp/{}/intents/*.json'.format(FILE_NAME, FILE_NAME))])
@@ -38,12 +48,12 @@ files = sorted([intent for intent in glob.glob('temp/{}/intents/*.json'.format(F
 # files.remove('template/intents/consideration-no_usersays_en.json')
 
 
-# In[ ]:
+# In[5]:
 
 
 # does not assume that all intents have usersays
 # assume English only
-intent_hold = "none" 
+intent_hold = "none"
 intent_jsons = []
 for file in files:
     intent_name = file.rpartition('.')[0]
@@ -54,29 +64,37 @@ for file in files:
         intent_jsons[-1].append(file)
 
 
-# In[ ]:
+# In[6]:
 
 
 intents = []
 for intent_json in intent_jsons:
-    try:
+#     try:
         intent = {}
         intent_info_json = intent_json[0]
         usersays_jsons = intent_json[1:] # should be one only
 
         with open(intent_info_json, encoding="utf-8") as f:
             intent_info = json.load(f)
+        
         intent["INTENT_NAME"] = intent_info["name"]
         intent["INPUT_CONTEXT"] = intent_info["contexts"]
         intent["OUTPUT_CONTEXT"] = [str(c["lifespan"]) + ", " + str(c["name"])  
                                     for c in intent_info["responses"][0]["affectedContexts"]]
-
-        response_list_or_str = intent_info["responses"][0]["messages"][0]["speech"]
-        if type(response_list_or_str) == str:
-            intent["RESPONSES"] = [response_list_or_str]
-        else: 
-            intent["RESPONSES"] = [m for m in response_list_or_str]
-
+        intent["RESPONSES"] = []
+        
+        for message in intent_info["responses"][0]["messages"]:
+            if message["type"] == 0:
+                if type(message["speech"]) == list:
+                    intent["RESPONSES"].append(message["speech"])
+                else:
+                    intent["RESPONSES"].append([message["speech"]])
+            elif message["type"] == 4:
+                intent["RESPONSES"].append([json.dumps(message["payload"])])
+            else:
+                pass
+#         print()
+        
         intent["USER_SAYS"] = []
         for usersays_json in usersays_jsons: # should be one or none
             with open(usersays_json, encoding="utf-8") as f:
@@ -86,27 +104,40 @@ for intent_json in intent_jsons:
         intents.append(intent)
     #     pp(intent_info)
     #     break
-    except:
+#     except:
         pass
 
 
-# In[ ]:
+# In[7]:
+
+
+MAX_TOTAL_RESPONSES = max([len(intent["RESPONSES"]) for intent in intents]+[1])
+# create empty arrays until they have the same number of arrays
+for intent in intents:
+    for _ in range(MAX_TOTAL_RESPONSES - len(intent["RESPONSES"])):
+        intent["RESPONSES"].append([])
+    pass
+
+
+# In[8]:
 
 
 intents
 
 
-# In[ ]:
+# In[9]:
 
 
 len_INPUT_CONTEXT = max([len(intent["INPUT_CONTEXT"]) for intent in intents]+[1])
 len_OUTPUT_CONTEXT = max([len(intent["OUTPUT_CONTEXT"]) for intent in intents]+[1])
 len_USER_SAYS = max([len(intent["USER_SAYS"]) for intent in intents]+[1])
-len_RESPONSES = max([len(intent["RESPONSES"]) for intent in intents]+[1])
-lengths = [1, len_INPUT_CONTEXT, len_INPUT_CONTEXT, len_USER_SAYS, len_RESPONSES]
+lens_RESPONSES = []  # find out the max number of variants of the i-th response
+for i in range(MAX_TOTAL_RESPONSES):
+    lens_RESPONSES.append(max([len(intent["RESPONSES"][i]) for intent in intents]+[1]))
+lengths = [1, len_INPUT_CONTEXT, len_OUTPUT_CONTEXT, len_USER_SAYS, *lens_RESPONSES]
 
 
-# In[ ]:
+# In[10]:
 
 
 row_length = sum(lengths)
@@ -115,10 +146,11 @@ columns[0] = "INTENT_NAME"
 columns[sum(lengths[:1])] = "INPUT_CONTEXT"
 columns[sum(lengths[:2])] = "OUTPUT_CONTEXT"
 columns[sum(lengths[:3])] = "USER_SAYS"
-columns[sum(lengths[:4])] = "RESPONSES"
+for i in range(MAX_TOTAL_RESPONSES):
+    columns[sum(lengths[:4+i])] = "RESPONSES"
 
 
-# In[ ]:
+# In[11]:
 
 
 row_list = []
@@ -128,22 +160,23 @@ for intent in intents:
     row[sum(lengths[:1]):sum(lengths[:1])+len(intent["INPUT_CONTEXT"])] = intent["INPUT_CONTEXT"]
     row[sum(lengths[:2]):sum(lengths[:2])+len(intent["OUTPUT_CONTEXT"])] = intent["OUTPUT_CONTEXT"]
     row[sum(lengths[:3]):sum(lengths[:3])+len(intent["USER_SAYS"])] = intent["USER_SAYS"]
-    row[sum(lengths[:4]):sum(lengths[:4])+len(intent["RESPONSES"])] = intent["RESPONSES"]
+    for i in range(MAX_TOTAL_RESPONSES):
+        row[sum(lengths[:4+i]):sum(lengths[:4+i])+len(intent["RESPONSES"][i])] = intent["RESPONSES"][i]
     row_list.append(row)
 
 
-# In[ ]:
+# In[12]:
 
 
 df = pd.DataFrame(row_list, columns=columns)
 df
 
 
-# In[ ]:
+# In[13]:
 
 
 df.to_csv("temp/{}.csv".format(FILE_NAME))
 
 
-# In[31]:
+# In[14]:
 
