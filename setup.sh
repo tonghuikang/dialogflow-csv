9bba80707d78d7ec8301545d9ec53179340e1d13
hkgcp <hkmac@df-and-csv-copy.c.ubtech-216703.internal>
2019-07-17 03:08:45 +0000

    duplicate on VM successful
    not dockerising now, going to convert all the chatbots

--- setup.sh
+++ setup.sh
@@ -5,5 +5,7 @@ sudo apt-get upgrade -y
 sudo apt-get install git zip python3 python3-pip python3-tk -y
 pip3 install jupyter ipykernel pandas
 git clone https://github.com/tonghuikang/dialogflow-csv
-chmod +x start.sh
-
+chmod +x ./dialogflow-csv/start.sh
+chmod +x ./dialogflow-csv/clr.sh
+touch ./dialogflow-csv/df-to-csv/token.pickle
+touch ./dialogflow-csv/df-to-csv/credentials.json
