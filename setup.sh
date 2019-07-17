cd48bf85d0cafef612b6b9f3414564172caf227d
Tong Hui Kang <tonghuikang@outlook.com>
2019-07-17 11:37:34 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ sudo apt-get upgrade -y
 sudo apt-get install git zip python3 python3-pip python3-tk -y
 pip3 install jupyter ipykernel pandas
 git clone https://github.com/tonghuikang/dialogflow-csv
+pip3 install -r dialogflow-csv/requirements.txt
 chmod +x ./dialogflow-csv/start.sh
 chmod +x ./dialogflow-csv/clr.sh
 touch ./dialogflow-csv/df-to-csv/token.pickle
